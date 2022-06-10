3de022a75699c0aac126a09e1db22b7025c72ac3
BlackyWerSonst <t.vogt@avsgmbh.de>
2022-06-10 14:09:05 +0200

    Feature update
    Added settings support
    Added animation support
    Added template support

--- widget/chat/js/script.js
+++ widget/chat/js/script.js
@@ -1,13 +1,10 @@
 /**
- * Message Sample data
+ * Message Template data
  * avatar: "https://static-cdn.jtvnw.net/jtv_user_pictures/a88dd690-f653-435e-ae3f-cd312ee5b736-profile_image-300x300.png"
- * badges: Array [ {…}, {…} ]
  * bits: 0
  * channel: "blackywersonst"
- * cheerEmotes: Array [{…}, {…}]
  * color: "#B33B19"
  * displayName: "Blackywersonst"
- * emotes: Array [{…}, {…}]
  * firstMessage: false
  * hasBits: false
  * internal: false
@@ -23,10 +20,13 @@
  * subscriber: true
  * userId: 27638012
  * username: "blackywersonst"
+ * time: 19:36
  */
 
 // General Variables
 var defaultChatColor = "#fff";
+var settings;
+var template;
 
 /**
  * Storing avatars that have been called to save api calls
@@ -34,13 +34,16 @@ var defaultChatColor = "#fff";
  */
 var avatars = {}
 
-window.addEventListener('load', (event) => {
+// Load settings, template and connect to ws
+$.getJSON("js/settings.json", function (json) {
+    settings = json;
+    template = document.querySelector('#message');
     connectws();
 });
 
 function connectws() {
     if ("WebSocket" in window) {
-        ws = new WebSocket("ws://localhost:8080/");
+        ws = new WebSocket(settings.websocketURL);
         bindEvents();
     }
 }
@@ -61,14 +64,14 @@ function bindEvents() {
         }));
     };
 
-    ws.onmessage = async(event) => {
+    ws.onmessage = async (event) => {
         const wsdata = JSON.parse(event.data);
 
         if (wsdata.event == null) {
             return;
         }
 
-        // Todo: Add ClearChat function 
+        // Todo: Add ClearChat function
 
         if (wsdata.event.source === 'Twitch' && wsdata.event.type === 'ChatMessage') {
             add_message(wsdata.data.message);
@@ -79,7 +82,7 @@ function bindEvents() {
     };
 
 
-    ws.onclose = function() {
+    ws.onclose = function () {
         console.log("Reconnecting")
         setTimeout(connectws, 10000);
     };
@@ -90,46 +93,64 @@ function bindEvents() {
  * @param {object} message
  */
 async function add_message(message) {
+
+
+    // Adding time variable
+    var today = new Date();
+    message.time = today.getHours() + ":" + today.getMinutes();
+
     const msg = new Promise((resolve, reject) => {
-            resolve(getProfileImage(message.username));
-        }).then(avatar => {
-            message.avatar = avatar;
-            return renderBadges(message);
-        }).then(bages => {
-            message.badges.markup = bages;
-            return renderEmotes(message);
-        })
+        resolve(getProfileImage(message.username));
+    }).then(avatar => {
+        message.avatar = avatar;
+        return renderBadges(message);
+    }).then(bages => {
+        message.badges = bages;
+        return renderEmotes(message);
+    })
         .then(msg => {
             $("#chat").append(renderMessage(msg));
-            // This could also call an Streamer.bot Action
-        }).catch(function(error) {
+            // Todo: Add a fade out option
+        }).catch(function (error) {
             console.error(error);
         });
 }
+
 
 /**
  * Render message with template
  * @param {object} message
  * @returns
  */
-function renderMessage(message) {
+function renderMessage(message = {}) {
 
     if (!message.color) {
         message.color = defaultChatColor;
     }
 
-    return `
-    <li class="msg">
-            <div class="avatar">
-                <img src="${message.avatar}">
-            </div>
-            <div class="message ">
-                <span class="badges">${message.badges.markup}</span>
-                <span class="name" style="--userColor:${message.color}">${message.displayName}</span>
-                <div class="content">${message.message}</div>
-            </div>
-        </li>
-        `;
+    // Add classes for animation to message
+    message.classes = "msg";
+
+    if (settings.animations.animation) {
+        message.classes += " animate__animated";
+
+        if (settings.animations.showAnimation) {
+            message.classes += " animate__" + settings.animations.showAnimation;
+        }
+
+        if (settings.animations.hidedelay != 0) {
+            // Todo: Add hide animation after
+            // message.classes += " animate__" + settings.animations.hideAnimation;
+
+        }
+
+    }
+
+    // Get template and populate
+    var tpl = template;
+
+    const pattern = /{{\s*(\w+?)\s*}}/g; // {property}
+    return tpl.innerHTML.replace(pattern, (_, token) => message[token] || '');
 }
 
 /**
@@ -171,7 +192,7 @@ async function getProfileImage(username) {
 
     // Check if avatar is already stored
     if (avatars.username) {
-        return avatars.username;
+        return `<img src="${avatars.username}"\>`;
     }
 
     return fetch(`https://decapi.me/twitch/avatar/${username}`)
@@ -180,7 +201,7 @@ async function getProfileImage(username) {
         })
         .then(avatar => {
             avatars[username] = avatar;
-            return avatar;
+            return `<img src="${avatar}"\>`;
         });
 
 }
