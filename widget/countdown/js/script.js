dc9bc30e1caddd9fe192829ed3060fb0281d35c3
Timo Vogt <t.vogt@avsgmbh.de>
2022-06-14 07:23:56 +0200

    Update script.js
    Updated Socket ids

--- widget/countdown/js/script.js
+++ widget/countdown/js/script.js
@@ -1,4 +1,3 @@
-
 var currentTimer;
 var remainingSeconds;
 var timer;
@@ -18,7 +17,7 @@ function connectws() {
 }
 
 function bindEvents() {
-    ws.onopen = function () {
+    ws.onopen = function() {
         console.debug('Websocket connected');
         ws.send(JSON.stringify({
             "request": "Subscribe",
@@ -27,11 +26,11 @@ function bindEvents() {
                     "Custom"
                 ],
             },
-            "id": "100"
+            "id": "countdown"
         }));
     }
 
-    ws.onmessage = function (event) {
+    ws.onmessage = function(event) {
         // grab message and parse JSON
         const msg = event.data;
         const wsdata = JSON.parse(msg);
@@ -74,7 +73,7 @@ function bindEvents() {
         }
     };
 
-    ws.onclose = function () {
+    ws.onclose = function() {
         // "connectws" is the function we defined previously
         setTimeout(connectws, 10000);
     };
@@ -97,7 +96,7 @@ function setupTimer(time, description = "") {
 }
 
 function startTimer() {
-    timer = setInterval(function () {
+    timer = setInterval(function() {
 
         if (remainingSeconds === 0) {
             clearInterval(timer);
@@ -141,8 +140,7 @@ function timerDone() {
             "id": "f600902f-0b5d-454d-8487-f51ff8a9e6e8", // Can be found in context menu of action
             "name": "Timer Finished"
         },
-        "id": "201"
+        "id": "countdown-finished"
     }));
 
 }
-
