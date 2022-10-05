c72282548c5c728ff5187da2d1d66a2971a53c69
Timo Vogt <blackywersonst@gmx.de>
2022-10-05 10:23:12 +0200

    Added sb functions

--- games/flip a coin/js/streamer.bot.js
+++ games/flip a coin/js/streamer.bot.js
@@ -1,3 +1,5 @@
+var ws;
+
 var sbSettings = {
     widget: "flip-a-coin",
     // The URL to your streamer.bot ws server
@@ -11,7 +13,7 @@ window.addEventListener("load", (event) => {
 function connectws() {
     if ("WebSocket" in window) {
         console.log("Connecting to Streamer.Bot");
-        ws = new WebSocket(settings.websocketURL);
+        ws = new WebSocket(sbSettings.websocketURL);
         bindEvents();
     }
 }
@@ -21,26 +23,73 @@ function bindEvents() {
         ws.send(
             JSON.stringify({
                 request: "Subscribe",
-                id: settings.widget,
+                id: sbSettings.widget,
                 events: {
-                    raw: ["Action"],
+                    general: ["Custom"],
                 },
             })
         );
     };
 
-    ws.onmessage = async (event) => {
+    ws.onmessage = async(event) => {
+
         const wsdata = JSON.parse(event.data);
 
         if (wsdata.status == "ok" || wsdata.event.source == null) {
             return;
         }
-
         console.debug(wsdata);
 
+        switch (wsdata.data.name) {
+            case "FlipCoin":
+                result = flipCoin();
+                sendAnswerInChat(wsdata.data.arguments[result]);
+                break;
+            case "FlipCoinPoll":
+                // Create a poll wait for poll to finish and then flip the coin
+                break;
+            default:
+                console.debug(wsdata.data.name);
+                break;
+        }
+
     }
+
+    ws.onclose = function() {
+        setTimeout(connectws, 10000);
+    };
+
 };
 
-ws.onclose = function () {
-    setTimeout(connectws, 10000);
-};
+
+
+function sendAnswerInChat(outcome) {
+    ws.send(
+        JSON.stringify({
+            request: "DoAction",
+            action: {
+                name: "FAC - Answer",
+            },
+            id: "FlipACoinAnswer",
+            args: {
+                choice: outcome,
+            },
+        })
+    );
+}
+
+function createPoll(heads, tails) {
+    ws.send(
+        JSON.stringify({
+            request: "DoAction",
+            action: {
+                name: "FAC - Create Poll",
+            },
+            id: "FlipACoinPoll",
+            args: {
+                choice1: heads,
+                choice2: tails,
+            },
+        })
+    );
+}
