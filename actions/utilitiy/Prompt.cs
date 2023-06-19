5cd0ffbcb32709a5777418be992fecb82260a4c4
Timo Vogt <blackywersonst@gmx.de>
2023-06-19 14:56:20 +0200

    Updated popup

--- actions/utilitiy/Prompt.cs
+++ actions/utilitiy/Prompt.cs
@@ -3,24 +3,30 @@ using System.Windows.Forms;
 
 public class CPHInline
 {
-  public bool UserInput()
+  public string UserInput()
   {
-
+    int inputs =  args.ContainsKey("inputs") ? Convert.ToInt32(args["inputs"].ToString()) : 1;
     string label = args["label"].ToString();
     string description = args["description"].ToString();
-
-    string promptValue = Prompt.ShowDialog(description, label);
-
-    CPH.SetArgument("output", promptValue);
-
-    return true;
+    string promptValue = Prompt.ShowInputDialog(inputs, description, label);
+    return promptValue;
   }
 
+  public bool ButtonInput()
+  {
+    string label = args["label"].ToString();
+    string description = args["description"].ToString();
+    string trueLable = args["trueLable"].ToString();
+    string falseLable = args["falseLable"].ToString();
+    string promptValue = Prompt.ShowButtonDialog(description, label, trueLable, falseLable);
+    CPH.SetArgument("output", promptValue);
+    return true;
+  }
 }
 
-public static class Prompt
+public class Prompt
 {
-  public static string ShowDialog(string text, string caption)
+  public static string ShowInputDialog(int inputs, string text, string caption)
   {
     Form prompt = new Form()
     {
@@ -56,12 +62,60 @@ public static class Prompt
     {
       prompt.Close();
     };
-
     prompt.Controls.Add(textBox);
     prompt.Controls.Add(confirmation);
     prompt.Controls.Add(textLabel);
     prompt.AcceptButton = confirmation;
-
     return prompt.ShowDialog() == DialogResult.OK ? textBox.Text : "";
   }
+
+  public static string ShowButtonDialog(string text, string caption, string trueLable, string falseLable)
+  {
+    Form prompt = new Form()
+    {
+      Width = 500,
+      Height = 150,
+      FormBorderStyle = FormBorderStyle.FixedDialog,
+      Text = caption,
+      StartPosition = FormStartPosition.CenterScreen,
+      TopMost = true
+    };
+    Label textLabel = new Label()
+    {
+      Left = 50,
+      Top = 20,
+      Width = 400,
+      Text = text
+    };
+    Button choice1 = new Button()
+    {
+      Text = trueLable,
+      Left = 250,
+      Width = 100,
+      Top = 70,
+      DialogResult = DialogResult.OK
+    };
+    Button choice2 = new Button()
+    {
+      Text = falseLable,
+      Left = 350,
+      Width = 100,
+      Top = 70,
+    };
+    choice1.Click += (sender, e) =>
+    {
+      prompt.Close();
+    };
+    choice2.Click += (sender, e) =>
+    {
+      prompt.Close();
+    };
+    prompt.Controls.Add(choice1);
+    prompt.Controls.Add(choice2);
+    prompt.Controls.Add(textLabel);
+    prompt.AcceptButton = choice1;
+    prompt.CancelButton = choice2;
+    return prompt.ShowDialog() == DialogResult.OK ? "true" : "false";
+  }
+
 }
