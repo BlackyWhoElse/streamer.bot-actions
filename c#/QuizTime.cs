019a71bb864518a17ed67e1394f3cf71f847ec0e
Timo Vogt <blackywersonst@gmx.de>
2024-07-30 15:45:22 +0200

    Sync

---
+++ c#/QuizTime.cs
@@ -0,0 +1,29 @@
+using System;
+
+
+public class Question
+{
+  public string question { get; set; }
+  public string solution { get; set; }
+  public bool correct { get; set; }
+
+  public TodoItem(string question, string solution, int correct, string[] answers)
+  {
+    title = title;
+    description = description;
+    correct = correct;
+    answers = answers;
+  }
+}
+
+  public class CPHInline
+{
+    public bool Execute()
+    {
+
+      // Load random question
+      // Setup Predictions
+      // Auto Reveal after its done
+        return true;
+    }
+}
